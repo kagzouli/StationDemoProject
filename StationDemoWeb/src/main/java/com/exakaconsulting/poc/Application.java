487bbd3a4aa8cbf8bbb9a42b02afbd58a4d88d22
kagzouli <kagzouli@yahoo.fr>
2018-03-08 21:53:17 +0100

    KAR001 : Init

--- StationDemoWeb/src/main/java/com/exakaconsulting/poc/Application.java
+++ StationDemoWeb/src/main/java/com/exakaconsulting/poc/Application.java
@@ -1,13 +1,17 @@
 package com.exakaconsulting.poc;
 
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
 import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @Configuration
+@ComponentScan({"com.exakaconsulting.poc.service" , "com.exakaconsulting.poc.web."})
+@EnableSwagger2
 public class Application {
 	
 	/** For swagger-ui **/
