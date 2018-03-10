b4aeaddb816c14fbc1dc223a312d14c484c771fa
kagzouli <kagzouli@yahoo.fr>
2018-03-10 10:36:43 +0100

    KAR001 : Init

--- StationDemoService/src/test/java/com/exakaconsulting/poc/TestStationDemoService.java
+++ StationDemoService/src/test/java/com/exakaconsulting/poc/TestStationDemoService.java
@@ -1,6 +1,5 @@
 package com.exakaconsulting.poc;
 
-import java.util.Arrays;
 import java.util.List;
 
 import org.apache.commons.lang3.StringUtils;
--- StationDemoWeb/src/main/java/com/exakaconsulting/poc/web/StationDemoController.java
+++ StationDemoWeb/src/main/java/com/exakaconsulting/poc/web/StationDemoController.java
@@ -1,9 +1,16 @@
 package com.exakaconsulting.poc.web;
 
+import java.util.Collections;
 import java.util.List;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
+import org.springframework.util.Assert;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -11,25 +18,65 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.exakaconsulting.poc.service.CriteriaSearchTrafficStation;
 import com.exakaconsulting.poc.service.IStationDemoService;
+import com.exakaconsulting.poc.service.StationDemoServiceImpl;
+import com.exakaconsulting.poc.service.TechnicalException;
 import com.exakaconsulting.poc.service.TrafficStationBean;
 
-@RestController("/station")
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+
+@CrossOrigin(origins="*" , allowedHeaders= "*" , exposedHeaders= {"Access-Control-Allow-Origin"}, methods={RequestMethod.GET , RequestMethod.POST, RequestMethod.PUT , RequestMethod.DELETE, RequestMethod.OPTIONS})
+@RestController
+@Api(value = "/", description = "This REST API is use to have informations about the traffic on the station.<br/>")
 public class StationDemoController {
+	
+	/** Logger **/
+	private static final Logger LOGGER = LoggerFactory.getLogger(StationDemoServiceImpl.class);
+
 	
 	@Autowired
 	private IStationDemoService stationDemoService;
 	
-	@RequestMapping(value = "/searchStation", method = { RequestMethod.GET}, consumes = {
-			MediaType.APPLICATION_FORM_URLENCODED_VALUE }, produces = { MediaType.APPLICATION_JSON_VALUE })
+	@ApiOperation(value = "This method is use to search a traffic stations by criteria", response = TrafficStationBean.class, responseContainer = "List")
+	@RequestMapping(value = "/findStationsByCrit", method = { RequestMethod.POST}, consumes = {
+			MediaType.APPLICATION_JSON_VALUE }, produces = { MediaType.APPLICATION_JSON_VALUE })
 	@ResponseBody
-	public List<TrafficStationBean> listSearchStations(){
-		
-		CriteriaSearchTrafficStation criteria = new CriteriaSearchTrafficStation();
-	
-		List<TrafficStationBean> listTrafficStation = stationDemoService.findStations(criteria);
-		return listTrafficStation;
-	}
+	public List<TrafficStationBean> listSearchStations(@RequestBody CriteriaSearchTrafficStation criteria){	
+		LOGGER.info("BEGIN of the method listSearchStations of the class " + StationDemoController.class.getName());
 
+		List<TrafficStationBean> listStationBean = Collections.emptyList();
+		try {
+			Assert.notNull(criteria, "The criteria must be set");
+			
+			listStationBean = this.stationDemoService.findStations(criteria);
+		} catch (Exception exception) {
+			LOGGER.error(exception.getMessage(), exception);
+			throw new TechnicalException(exception);
+		}
+		LOGGER.info("END of the method listSearchStations of the class " + StationDemoController.class.getName());
+		return listStationBean;
+	}
+	
+	
+	@RequestMapping(value = "/findStationById/{id}", method = { RequestMethod.GET}, produces = { MediaType.APPLICATION_JSON_VALUE })
+	@ResponseBody
+	public TrafficStationBean findTrafficStationById(@PathVariable final Integer id){
+
+		LOGGER.info("BEGIN of the method findTrafficStationById of the class " + StationDemoController.class.getName());
+		TrafficStationBean trafficStationBean =  null;
+		try {
+			Assert.notNull(id, "The id must be set");
+			
+			trafficStationBean = this.stationDemoService.findStationById(id);
+		} catch (Exception exception) {
+			LOGGER.error(exception.getMessage(), exception);
+			throw new TechnicalException(exception);
+		}
+		LOGGER.info("END of the method findTrafficStationById of the class " + StationDemoController.class.getName());
+
+		return trafficStationBean; 
+		
+	}
 	
 
 }
