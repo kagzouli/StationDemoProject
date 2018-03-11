6c98044bb379ed1b6d8d40ff6faa6f6842fe4059
unknown <koko_266@@hotmail.fr>
2018-03-11 22:13:23 +0100

    KAR003 : Add spring security

--- StationDemoClient/station-angular4-poc/src/app/component/search-station/search-station.component.ts
+++ StationDemoClient/station-angular4-poc/src/app/component/search-station/search-station.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+
+
 @Component({
   selector: 'app-search-station',
   templateUrl: './search-station.component.html',
@@ -7,9 +10,31 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SearchStationComponent implements OnInit {
 
-  constructor() { }
+  displayedColumns = ['id','reseau', 'station', 'traffic', 'corresp', 'ville', 'arrond'];
+  
+
+  rForm: FormGroup;
+
+  launchAction : boolean = false;
+    
+
+  constructor(private fb: FormBuilder) { 
+
+    this.rForm = fb.group({
+      'reseau' : [null, Validators.compose([Validators.maxLength(64)])],
+      'station' : [null, ],
+      'trafficMin' : [null, ],
+      'trafficMax' : [null,  ],
+      'ville' : [null, ],
+    });
+
+  }
 
   ngOnInit() {
+  }
+
+  disableButton(invalidform : boolean){
+    return invalidform || this.launchAction; 
   }
 
 }
