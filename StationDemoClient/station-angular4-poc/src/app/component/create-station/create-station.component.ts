2ff5c7ee2a670ff98a37f84b6a16cdae8220a729
unknown <koko_266@@hotmail.fr>
2018-03-12 21:33:21 +0100

    KAR005 : Add new component not finish create-station

--- StationDemoClient/station-angular4-poc/src/app/component/create-station/create-station.component.ts
+++ StationDemoClient/station-angular4-poc/src/app/component/create-station/create-station.component.ts
@@ -1,5 +1,11 @@
 import { Component, OnInit } from '@angular/core';
 
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+
+import {StringMapEntry} from '../../bean/stringmapentry';
+
+
+
 @Component({
   selector: 'app-create-station',
   templateUrl: './create-station.component.html',
@@ -7,9 +13,37 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CreateStationComponent implements OnInit {
 
-  constructor() { }
+  rForm: FormGroup;
+  
+  launchAction : boolean = false;
+
+  
+  
+  mapReseaux: StringMapEntry[] = [ new StringMapEntry('Metro' , 'Metro') , new StringMapEntry('RER' , 'RER')];
+  
+  reseauChoose: string = '';
+  
+  
+
+  constructor(private fb: FormBuilder) { 
+
+    this.rForm = fb.group({
+      'reseau' : [null, Validators.compose([Validators.required])],
+      'station' : [null, ],
+      'traffic' : [null, ],
+      'correspondance' : [null,  ],
+      'ville' : [null, ],
+      'arrondissement' : [null, ]
+    });
+
+
+  }
 
   ngOnInit() {
+  }
+
+  disableButton(invalidform : boolean){
+    return invalidform || this.launchAction; 
   }
 
 }
