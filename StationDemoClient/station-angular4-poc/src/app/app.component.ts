46743e7266fdcec4b6d2a64b4e2d16918f7b352b
unknown <koko_266@@hotmail.fr>
2018-03-25 21:17:01 +0200

    KAR014 : Ne marche pas

--- StationDemoClient/station-angular4-poc/src/app/app.component.ts
+++ StationDemoClient/station-angular4-poc/src/app/app.component.ts
@@ -12,10 +12,10 @@ export class AppComponent {
   title = 'app';
 
   constructor(private oauthService: OAuthService) {
-    this.oauthService.clientId = '0oaefscbvcNZ5dpSU0h7';
+    this.oauthService.clientId = '0oaeg3yghaL9mQalz0h7';
     this.oauthService.scope = 'openid profile email';
     this.oauthService.issuer = 'https://dev-884254.oktapreview.com/oauth2/default';
-    this.oauthService.redirectUri = 'http://localhost:4200',
+    this.oauthService.redirectUri = 'http://localhost:4200/',
     this.oauthService.tokenValidationHandler = new JwksValidationHandler();
     // Load Discovery Document and then try to login the user
     this.oauthService.loadDiscoveryDocument().then(() => {
--- StationDemoClient/station-angular4-poc/src/app/component/search-station/search-station.component.ts
+++ StationDemoClient/station-angular4-poc/src/app/component/search-station/search-station.component.ts
@@ -195,6 +195,3 @@ export class SearchStationComponent implements OnInit {
   }
 }
 
-
-   
-}
