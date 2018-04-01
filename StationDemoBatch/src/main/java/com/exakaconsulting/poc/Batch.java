360f801757891550c4d7f3804744c969cd8883ff
unknown <koko_266@@hotmail.fr>
2018-04-01 16:31:06 +0200

    KAR049 : Spring batch

--- StationDemoBatch/src/main/java/com/exakaconsulting/poc/Batch.java
+++ StationDemoBatch/src/main/java/com/exakaconsulting/poc/Batch.java
@@ -2,8 +2,10 @@ package com.exakaconsulting.poc;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.PropertySource;
 
 @SpringBootApplication
+@PropertySource("classpath:configServices.properties")
 public class Batch {
 	
 	
