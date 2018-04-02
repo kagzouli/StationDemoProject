6e5d413aee69a247d0cfbf34b817c737ce593d72
unknown <koko_266@@hotmail.fr>
2018-04-02 19:13:06 +0200

    KAR052 : Correction batch

--- StationDemoBatch/src/main/java/com/exakaconsulting/poc/TrafficStationPreparedStatement.java
+++ StationDemoBatch/src/main/java/com/exakaconsulting/poc/TrafficStationPreparedStatement.java
@@ -1,19 +1,30 @@
 package com.exakaconsulting.poc;
 
+import java.io.UnsupportedEncodingException;
+
 import java.sql.PreparedStatement;
 import java.sql.SQLException;
 
 import org.apache.commons.lang3.StringUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.batch.item.database.ItemPreparedStatementSetter;
 
+import com.exakaconsulting.poc.service.IStationDemoService;
 import com.exakaconsulting.poc.service.TrafficStationBean;
 
+
+
 public class TrafficStationPreparedStatement implements ItemPreparedStatementSetter<TrafficStationBean> {
-	 
+
+	private static final Logger LOGGER = LoggerFactory.getLogger(TrafficStationPreparedStatement.class);
+
     @Override
     public void setValues(TrafficStationBean trafficStationBean, 
                           PreparedStatement preparedStatement) throws SQLException {
-        preparedStatement.setString(1, trafficStationBean.getReseau());
+    	
+    	final String reseau = StringUtils.endsWith(trafficStationBean.getReseau() , "tro") ? "Metro" : trafficStationBean.getReseau();
+        preparedStatement.setString(1, reseau);
         preparedStatement.setString(2, trafficStationBean.getStation());
         preparedStatement.setLong(3, trafficStationBean.getTraffic());
  
@@ -23,7 +34,12 @@ public class TrafficStationPreparedStatement implements ItemPreparedStatementSet
         	preparedStatement.setString(4, "");
         }
         
-        preparedStatement.setString(5, trafficStationBean.getVille());
+        try {
+			preparedStatement.setString(5, new String(trafficStationBean.getVille().getBytes(), "UTF-8"));
+		} catch (UnsupportedEncodingException exception) {
+			preparedStatement.setString(5, "");
+			LOGGER.warn(exception.getMessage() , exception);
+		}
         
         if (trafficStationBean.getArrondissement() != null){
         	preparedStatement.setInt(6, trafficStationBean.getArrondissement());
