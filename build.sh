4954edd73ab39ea98d0b228a2dea4237909140ea
unknown <koko_266@@hotmail.fr>
2019-12-27 20:29:30 +0100

    Correction docker

--- build.sh
+++ build.sh
@@ -1,12 +1,15 @@
 /usr/local/maven/apache-maven-3.3.9/bin/mvn clean install
 echo "Build maven install successful"
-docker build -t station-db StationDemoDb/ 
+docker build -t kagzouli/station-db -f StationDemoDb/docker
 echo "Lancement image stationdb"
-docker build -t station-back StationDemoWeb/docker 
+cp StationDemoWeb/target/StationDemoSecureWeb.war StationDemoWeb/docker/StationDemoSecureWeb.war
+docker build -t kagzouli/station-back StationDemoWeb/docker
 echo "Lancement image stationback"
-docker build -t station-front-nginx StationDemoClient/station-angular4-poc/dockerwithnginx/ 
+cp -rf StationDemoClient/station-angular4-poc/dist StationDemoClient/station-angular4-poc/dockerwithnginx/
+docker build -t kagzouli/station-front-nginx StationDemoClient/station-angular4-poc/dockerwithnginx
 echo "Lancement image nginxstationfront"
-docker build -t station-front-tomcat StationDemoClient/station-angular4-poc/dockerwithtomcat/ 
+cp  StationDemoClient/station-angular4-poc/target/station-angular4-poc.war StationDemoClient/station-angular4-poc/dockerwithtomcat/station-angular4-poc.war
+docker build -t kagzouli/station-front-tomcat StationDemoClient/station-angular4-poc/dockerwithtomcat
 echo "Lancement image tomcatstationfront"
 
 
