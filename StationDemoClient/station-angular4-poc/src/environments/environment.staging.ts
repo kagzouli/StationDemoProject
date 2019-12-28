f2e7d264b621a8aa1224f6ffd5cfc709a6c2304a
unknown <koko_266@@hotmail.fr>
2019-12-28 21:07:57 +0100

    Configuration externalise

--- StationDemoClient/station-angular4-poc/src/environments/environment.staging.ts
+++ StationDemoClient/station-angular4-poc/src/environments/environment.staging.ts
@@ -1,14 +1,4 @@
 export const environment = {
-    production: false,
-
-    // Client id for okta
-    clientIdTrafStat: '0oaeg3yghaL9mQalz0h7',
-
-    // Okta URL
-    oktaUrl: 'https://dev-884254.oktapreview.com',
-  
-  
-     // Contexte pour le back End trafficStation
-    contextPathTrafStation: 'http://51.254.36.120:4000/StationDemoSecureWeb'
+    production: false
   };
   
--- StationDemoClient/station-angular4-poc/src/environments/environment.ts
+++ StationDemoClient/station-angular4-poc/src/environments/environment.ts
@@ -4,15 +4,5 @@
 // The list of which env maps to which file can be found in `.angular-cli.json`.
 
 export const environment = {
-  production: false,
-
-  // Client id for okta
-  clientIdTrafStat: '0oaeg3yghaL9mQalz0h7',
-
-  // Okta URL
-  oktaUrl: 'https://dev-884254.oktapreview.com',
-     
-  
-  contextPathTrafStation: 'http://localhost:8080/StationDemoSecureWeb',
-
+  production: false
 };
