5faf4237f16baa9345f8af30e3862ab1f6f08e64
unknown <koko_266@@hotmail.fr>
2020-10-01 20:59:13 +0200

    Correct

--- terraform/main.tf
+++ terraform/main.tf
@@ -106,7 +106,7 @@ resource "aws_route_table" "station_privateroutetable" {
   vpc_id = aws_vpc.station_vpc.id
   route {
     cidr_block = "0.0.0.0/0"
-    nat_gateway_id = aws_nat_gateway.station_natgwpublic1.id
+    nat_gateway_id = aws_nat_gateway.station_natgwpublic.id
   }
   tags = {
     Name = "station_privateroutable",
