dccf1a1520c035ab0f5c9141620f8e346dda2ee6
unknown <koko_266@@hotmail.fr>
2020-10-01 20:18:49 +0200

    On public2

--- terraform/main.tf
+++ terraform/main.tf
@@ -68,6 +68,26 @@ resource "aws_subnet" "station_publicsubnet2" {
     }
 }
 
+resource "aws_route_table" "station_public2routetable" {
+  vpc_id = aws_vpc.station_vpc.id
+  tags = {
+     Name = "station_public2routetable",
+     Application= var.application
+  }
+}
+
+resource "aws_route" "station_public2" {
+  route_table_id         = "${aws_route_table.station_public2routetable.id}"
+  destination_cidr_block = "0.0.0.0/0"
+  gateway_id             = "${aws_internet_gateway.station_internalgw.id}"
+}
+
+resource "aws_route_table_association" "station_routetablassoc_public2" {
+  subnet_id      = "${aws_subnet.station_publicsubnet2.id}"
+  route_table_id = "${aws_route_table.station_public2routetable.id}"
+}
+
+
 /*** Private subnet 1 **/
 resource "aws_subnet" "station_privatesubnet1" {
     vpc_id = aws_vpc.station_vpc.id
