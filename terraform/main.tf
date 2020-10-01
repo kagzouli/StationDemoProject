daed080d8ec756d2ca31993ca9859fe8fa89a616
unknown <koko_266@@hotmail.fr>
2020-10-01 19:43:22 +0200

    Zone 1 and Zone 2

--- terraform/main.tf
+++ terraform/main.tf
@@ -16,7 +16,7 @@ resource "aws_subnet" "station_publicsubnet1" {
     vpc_id = "${aws_vpc.station_vpc.id}"
 
     cidr_block =  var.public_subnet1_cidr
-    availability_zone = var.region
+    availability_zone = var.az_zone1
 
     tags = {
        Name = "station_publicsubnet1",
@@ -31,7 +31,7 @@ resource "aws_subnet" "station_publicsubnet2" {
     vpc_id = "${aws_vpc.station_vpc.id}"
 
     cidr_block =  var.public_subnet2_cidr
-    availability_zone = var.region
+    availability_zone = var.az_zone2
 
     tags = {
        Name = "station_publicsubnet2",
@@ -46,7 +46,7 @@ resource "aws_subnet" "station_privatesubnet1" {
     vpc_id = "${aws_vpc.station_vpc.id}"
 
     cidr_block =  var.private_subnet1_cidr
-    availability_zone = var.region
+    availability_zone = var.az_zone1
 
     tags = {
        Name = "station_privatesubnet1",
@@ -61,7 +61,7 @@ resource "aws_subnet" "station_privatesubnet2" {
     vpc_id = "${aws_vpc.station_vpc.id}"
 
     cidr_block =  var.private_subnet2_cidr
-    availability_zone = var.region
+    availability_zone = var.az_zone2
 
     tags = {
        Name = "station_privatesubnet2",
--- terraform/vars.tf
+++ terraform/vars.tf
@@ -30,6 +30,16 @@ variable "private_subnet2_cidr" {
     default = "12.0.4.0/26"
 }
 
+variable "az_zone1" {
+    description = "Availability zone 1"
+    default = "eu-west-3a"
+}
+
+variable "az_zone2" {
+    description = "Availability zone 1"
+    default = "eu-west-3b"
+}
+
 
 variable "application"{ 
   default     = "STATION"
