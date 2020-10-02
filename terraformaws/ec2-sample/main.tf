1bd370ce30d79681bb435a3fff04271c76213ef1
unknown <koko_266@@hotmail.fr>
2020-10-02 17:23:38 +0200

    Init ec2-simpl

--- terraformaws/ec2-sample/main.tf
+++ terraformaws/ec2-sample/main.tf
@@ -2,6 +2,7 @@
 resource "aws_instance" "nginx_front" {
 	ami = aws_ami.amazon-linux-2.id
 	instance_type = "t2.nano"
+    associate_public_ip_address = true
     subnet_id     = aws_subnet.station_publicsubnet1.id
     user_data     = file("install_nginx_front.sh")
 	tags = {
