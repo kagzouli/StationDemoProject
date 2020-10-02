269ead9bf9fbc375ea1f1bc4a044cc0abf68730d
unknown <koko_266@@hotmail.fr>
2020-10-02 18:09:43 +0200

    Correct

--- terraformaws/ec2-sample/install_nginx_front.sh
+++ terraformaws/ec2-sample/install_nginx_front.sh
@@ -1,19 +1,6 @@
 #!/bin/bash
-# Install docker
-apt-get update
-apt-get install -y apt-transport-https ca-certificates curl software-properties-common
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
-add-apt-repository \
-   "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
-   $(lsb_release -cs) \
-   stable"
-apt-get update
-apt-get install -y docker-ce
-usermod -aG docker ubuntu
-
-# Install docker-compose
-curl -L https://github.com/docker/compose/releases/download/1.21.0/docker-compose-$(uname -s)-$(uname -m) -o /usr/local/bin/docker-compose
-chmod +x /usr/local/bin/docker-compose
-
-# Docker pull station_front
-docker pull kagzouli/station-db:1.0.6
+sudo apt-get update
+sudo apt-get install -y apache2
+sudo systemctl start apache2
+sudo systemctl enable apache2
+echo "<h1>Deployed via Terraform</h1>" | sudo tee /var/www/html/index.html
