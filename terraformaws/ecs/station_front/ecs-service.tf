2ef35b3e1b468bf62f68044ac9918ab0c753c11d
unknown <koko_266@@hotmail.fr>
2020-10-02 23:10:20 +0200

    Task definition

--- terraformaws/ecs/station_front/ecs-service.tf
+++ terraformaws/ecs/station_front/ecs-service.tf
@@ -1,10 +1,9 @@
 resource "aws_ecs_service" "station_front_ecs_service"{
     name                =  "station_front_ecs_service"
-    task_definition     = aws_ecs_task_definition.station_front_ecs_task_definition.revision
+    task_definition     = "${aws_ecs_task_definition.station_front_ecs_task_definition.family}:${max(aws_ecs_task_definition.station_front_ecs_task_definition.revision, aws_ecs_task_definition.station_front_ecs_task_definition.revision)}
     cluster             = aws_ecs_cluster.station_front_ecs_cluster.name
     desired_count       = 1
     launch_type         = "FARGATE"
-    propagate_tags      = "SERVICE"
  #   health_check_grace_period_seconds = 240
 
     network_configuration{
