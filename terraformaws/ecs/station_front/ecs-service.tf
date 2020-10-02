e689b44d165c1e298d833969663aeffc09da5b97
unknown <koko_266@@hotmail.fr>
2020-10-02 22:41:19 +0200

    Task definition

--- terraformaws/ecs/station_front/ecs-service.tf
+++ terraformaws/ecs/station_front/ecs-service.tf
@@ -2,13 +2,19 @@ resource "aws_ecs_service" "station_front_ecs_service"{
     name                =  "station_front_ecs_service"
     task_definition     = aws_ecs_task_definition.station_front_ecs_task_definition.arn
     cluster             = aws_ecs_cluster.station_front_ecs_cluster.name
+    desired_count       = 1
     launch_type         = "EC2"
-    assign_public_ip    = true
+    enable_ecs_managed_tags = true
  #   health_check_grace_period_seconds = 240
 
     network_configuration{
         subnets             = var.public_subnets
         security_groups     = [aws_security_group.station_front.id]
+    }
+
+    tags = {
+        Name = "station_front_ecs_service"
+        Application= var.application
     }
 
     
