b881be3a5f0df5e02b08ef99cc9ce1dd3443ac4a
unknown <koko_266@@hotmail.fr>
2020-10-02 17:44:43 +0200

    Init ec2-simpl

--- terraformaws/ec2-sample/main.tf
+++ terraformaws/ec2-sample/main.tf
@@ -1,3 +1,29 @@
+# Create security groups for nginx-front.
+resource "aws_security_group" "allows_http" {
+  name        = "allows_http"
+  description = "Allow Http request"
+  vpc_id      = data.aws_vpc.station_vpc.id
+
+  ingress {
+    protocol    = "HTTP"
+    from_port   = 80
+    to_port     = 80
+    cidr_blocks = ["0.0.0.0/0"]
+    description = "Http request"
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "allow_http"
+  }
+}
+
 // Nginx front server
 resource "aws_instance" "nginx_front" {
 	ami = data.aws_ami.ubuntu.id
@@ -5,8 +31,11 @@ resource "aws_instance" "nginx_front" {
     associate_public_ip_address = true
     subnet_id     = data.aws_subnet.station_publicsubnet1.id
     user_data     = file("ec2-sample/install_nginx_front.sh")
-	tags = {
+	security_groups = aws_security_group.allows_http.id
+    tags = {
 		Name = "nginx-front"	
 		Batch = "5AM"
 	}
+
 }
+
