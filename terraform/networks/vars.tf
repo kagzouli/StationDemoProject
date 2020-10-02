472edebc0ccd45fa81be752e39b27760fcd5101d
unknown <koko_266@@hotmail.fr>
2020-10-02 10:30:04 +0200

    Variabilisation

--- terraform/networks/vars.tf
+++ terraform/networks/vars.tf
@@ -9,38 +9,38 @@ variable "vpc_cidr_block" {
 }
 
 variable "public_subnet1_cidr" {
-    type        = "string"
+    type        = string
     description = "CIDR for the Public Subnet1"
 }
 
 variable "public_subnet2_cidr" {
-    type        = "string"
+    type        = string
     description = "CIDR for the Public Subnet2"
 }
 
 variable "private_subnet1_cidr" {
-    type        = "string"
+    type        =  string
     description = "CIDR for the Private Subnet1"
 }
 
 variable "private_subnet2_cidr" {
-    type        = "string"
-    description = "CIDR for the Private Subnet2"
+    type        =  string
+    description =  "CIDR for the Private Subnet2"
 }
 
 variable "az_zone1" {
-    type        = "string"
-    description = "Availability zone 1"
+    type        =  string
+    description =  "Availability zone 1"
 }
 
 variable "az_zone2" {
-    type        = "string"
-    description = "Availability zone 1"
+    type        =  string
+    description =  "Availability zone 2"
 }
 
 
 variable "application"{ 
-  type        = string
+  type        =  string
   description = "Application"
   default     = "STATION"
 }
