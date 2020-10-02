902f84d7a1264b2f8a6c972daeb4623ec4c919c0
unknown <koko_266@@hotmail.fr>
2020-10-02 20:47:15 +0200

    Correction

--- terraformaws/ecs/station_front/ecs-service.tf
+++ terraformaws/ecs/station_front/ecs-service.tf
@@ -2,7 +2,7 @@ resource "aws_ecs_service" "station_front_ecs_service"{
     name =  "station_front_ecs_service"
     cluster = aws_ecs_cluster.station_front_ecs_cluster.name
     launch_type = "EC2"
-    health_check_grace_period_second = 240
+    health_check_grace_period_seconds = 240
     network_configuration{
         subnets    = var.public_subnets
         security_groups = aws_security_group.station_front.id
