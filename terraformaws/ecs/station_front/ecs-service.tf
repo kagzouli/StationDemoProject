9d8f12fe9304070d03c1225e7c806415e2f7dcd9
unknown <koko_266@@hotmail.fr>
2020-10-02 20:34:13 +0200

    Correction

--- terraformaws/ecs/station_front/ecs-service.tf
+++ terraformaws/ecs/station_front/ecs-service.tf
@@ -1,17 +1,3 @@
+//resource "aws_ecs_service" "station_front_ecs_service"{
 
-
-// Nginx front server
-resource "aws_instance" "nginx_front" {
-	ami = data.aws_ami.ubuntu.id
-	instance_type = "t2.micro"
-    associate_public_ip_address = true
-    subnet_id     = var.public_subnet1_id
-    user_data     = file("ec2-sample/install_nginx_front.sh")
-	security_groups = [aws_security_group.allows_http.id]
-    tags = {
-		Name = "nginx-front"	
-		Application= var.application
-	}
-
-}
-
+//}
