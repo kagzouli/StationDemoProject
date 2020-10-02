0b402e3a9626fe1dae6dbedf18c72247f25063a2
unknown <koko_266@@hotmail.fr>
2020-10-02 23:26:40 +0200

    Task definition

--- terraformaws/ecs/station_front/ecs-iam.tf
+++ terraformaws/ecs/station_front/ecs-iam.tf
@@ -24,3 +24,23 @@ EOF
    Application= var.application
   }
 }
+
+resource "aws_iam_role" "station_front_execution_role" {
+  name               = "station_front_execution_role"
+  assume_role_policy = data.aws_iam_policy_document.station_front_execution_role_policy.json
+  tags = {
+        Name = "station_front_execution_role"
+        Application= var.application
+  }
+}
+
+data "aws_iam_policy_document" "station_front_execution_role_policy" {
+  statement {
+    actions = ["sts:AssumeRole"]
+
+    principals {
+      type        = "Service"
+      identifiers = ["ecs-tasks.amazonaws.com"]
+    }
+  }
+}
