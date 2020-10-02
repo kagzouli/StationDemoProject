fa0ceb6559797cdfd630f9391478d9f5b60ea05a
unknown <koko_266@@hotmail.fr>
2020-10-02 21:28:11 +0200

    Task definition

--- terraformaws/ecs/main.tf
+++ terraformaws/ecs/main.tf
@@ -1,12 +1,13 @@
 module "station_front"{
      source = "./station_front"
-     application = var.application
-     vpc_id = data.aws_vpc.station_vpc.id
-     public_subnets = [ data.aws_subnet.station_publicsubnet1.id , data.aws_subnet.station_publicsubnet2.id ]
+     application                    = var.application
+     vpc_id                         = data.aws_vpc.station_vpc.id
+     public_subnets                 = [ data.aws_subnet.station_publicsubnet1.id , data.aws_subnet.station_publicsubnet2.id ]
      station_front_container_memory = var.station_front_container_memory
-     station_front_container_cpu = var.station_front_container_cpu
-     station_front_image  = var.station_front_image
-     station_front_host_port = var.station_front_host_port
+     station_front_container_cpu    = var.station_front_container_cpu
+     station_front_image            = var.station_front_image
+     station_front_host_port        = var.station_front_host_port
+     region                         = var.region
 
 
 }
