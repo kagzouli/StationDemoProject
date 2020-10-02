b0b8cdb8db13d7e19d21150960fa1b047eb1425a
unknown <koko_266@@hotmail.fr>
2020-10-02 20:16:51 +0200

    Correction

--- terraformaws/ec2-sample/main.tf
+++ terraformaws/ec2-sample/main.tf
@@ -30,7 +30,7 @@ resource "aws_instance" "nginx_front" {
 	ami = data.aws_ami.ubuntu.id
 	instance_type = "t2.micro"
     associate_public_ip_address = true
-    subnet_id     = data.aws_subnet.station_publicsubnet1.id
+    subnet_id     = public_subnet1_id
     user_data     = file("ec2-sample/install_nginx_front.sh")
 	security_groups = [aws_security_group.allows_http.id]
     tags = {
--- terraformaws/ecs/main.tf
+++ terraformaws/ecs/main.tf
@@ -1,4 +1,6 @@
 module "station_front"{
      source = "./station_front"
      application = var.application
+     public_subnet1_id= data.aws_subnet.station_publicsubnet1.id
+     public_subnet2_id= data.aws_subnet.station_publicsubnet2.id
 }
