22659fd93477a93a468fc5009895e0468157e773
unknown <koko_266@@hotmail.fr>
2020-10-02 17:45:33 +0200

    Init ec2-simpl

--- terraformaws/ec2-sample/main.tf
+++ terraformaws/ec2-sample/main.tf
@@ -31,7 +31,7 @@ resource "aws_instance" "nginx_front" {
     associate_public_ip_address = true
     subnet_id     = data.aws_subnet.station_publicsubnet1.id
     user_data     = file("ec2-sample/install_nginx_front.sh")
-	security_groups = aws_security_group.allows_http.id
+	security_groups = [aws_security_group.allows_http.id]
     tags = {
 		Name = "nginx-front"	
 		Batch = "5AM"
