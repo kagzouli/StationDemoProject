df4d9ee74b81005be90cd7935fb68b3b0347bba7
unknown <koko_266@@hotmail.fr>
2020-10-02 22:30:22 +0200

    Task definition

--- terraformaws/ecs/station_front/ecs-service.tf
+++ terraformaws/ecs/station_front/ecs-service.tf
@@ -3,7 +3,7 @@ resource "aws_ecs_service" "station_front_ecs_service"{
     task_definition     = aws_ecs_task_definition.station_front_ecs_task_definition.arn
     cluster             = aws_ecs_cluster.station_front_ecs_cluster.name
     launch_type         = "EC2"
-    health_check_grace_period_seconds = 240
+ #   health_check_grace_period_seconds = 240
 
     network_configuration{
         subnets             = var.public_subnets
