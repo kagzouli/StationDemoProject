332cd64c729c686f31446d1135427ce6dd6639a4
unknown <koko_266@@hotmail.fr>
2020-10-02 17:33:09 +0200

    Init ec2-simpl

--- terraformaws/ec2-sample/main.tf
+++ terraformaws/ec2-sample/main.tf
@@ -6,7 +6,7 @@ resource "aws_instance" "nginx_front" {
     subnet_id     = data.aws_subnet.station_publicsubnet1.id
     user_data     = file("ec2-sample/install_nginx_front.sh")
 	tags = {
-		Name = "Terraform"	
+		Name = "nginx-front"	
 		Batch = "5AM"
 	}
 }
