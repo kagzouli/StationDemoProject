9bb1226576fe54e4e813eec4cc8e8bb9b0cdc0b1
unknown <koko_266@@hotmail.fr>
2020-10-02 17:08:07 +0200

    Init ec2-simpl

--- terraformaws/ec2-sample/main.tf
+++ terraformaws/ec2-sample/main.tf
@@ -4,13 +4,13 @@ resource "aws_instance" "nginx_front" {
 	instance_type = "t2.nano"
     subnet_id     = aws_subnet.station_publicsubnet1.id
 	user_data = << EOF
-		#! /bin/bash
-        sudo apt-get update
-		sudo apt-get install -y apache2
-		sudo systemctl start apache2
-		sudo systemctl enable apache2
-		echo "<h1>Deployed via Terraform</h1>" | sudo tee /var/www/html/index.html
-	EOF
+     #! /bin/bash
+    sudo apt-get update
+    sudo apt-get install -y apache2
+    sudo systemctl start apache2
+    sudo systemctl enable apache2
+    echo "<h1>Deployed via Terraform</h1>" | sudo tee /var/www/html/index.html
+    EOF
 	tags = {
 		Name = "Terraform"	
 		Batch = "5AM"
