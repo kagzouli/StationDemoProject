57d295db390724f676c7208386c75be013fa89a9
unknown <koko_266@@hotmail.fr>
2020-10-02 10:28:24 +0200

    Variabilisation

--- terraform/networks/vars.tf
+++ terraform/networks/vars.tf
@@ -1,48 +1,46 @@
 variable "region" {
-  default     = "eu-west-3"
   type        = string
   description = "Region of the VPC"
 }
 
 variable "vpc_cidr_block" {
-  default     = "12.0.0.0/21"
   type        = string
   description = "CIDR of the VPC"
 }
 
 variable "public_subnet1_cidr" {
+    type        = "string"
     description = "CIDR for the Public Subnet1"
-    default = "12.0.1.0/27"
 }
 
 variable "public_subnet2_cidr" {
+    type        = "string"
     description = "CIDR for the Public Subnet2"
-    default = "12.0.2.0/27"
 }
 
 variable "private_subnet1_cidr" {
+    type        = "string"
     description = "CIDR for the Private Subnet1"
-    default = "12.0.3.0/26"
 }
 
 variable "private_subnet2_cidr" {
+    type        = "string"
     description = "CIDR for the Private Subnet2"
-    default = "12.0.4.0/26"
 }
 
 variable "az_zone1" {
+    type        = "string"
     description = "Availability zone 1"
-    default = "eu-west-3a"
 }
 
 variable "az_zone2" {
+    type        = "string"
     description = "Availability zone 1"
-    default = "eu-west-3b"
 }
 
 
 variable "application"{ 
-  default     = "STATION"
   type        = string
   description = "Application"
+  default     = "STATION"
 }
