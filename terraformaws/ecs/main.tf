e957f130f89fc75f1f159063cc3580f40914adbf
unknown <koko_266@@hotmail.fr>
2020-10-03 09:09:41 +0200

    Test all

--- terraformaws/ecs/main.tf
+++ terraformaws/ecs/main.tf
@@ -1,17 +1,4 @@
-/*module "station_front"{
-     source = "./station_front"
-     application                    = var.application
-     vpc_id                         = data.aws_vpc.station_vpc.id
-     subnets_id                     = [ data.aws_subnet.station_publicsubnet1.id , data.aws_subnet.station_publicsubnet2.id ]
-     station_front_container_memory = var.station_front_container_memory
-     station_front_container_cpu    = var.station_front_container_cpu
-     station_front_image            = var.station_front_image
-     station_front_host_port        = var.station_front_host_port
-     region                         = var.region
-     task_role_arn                  = aws_iam_role.station_iam_role.arn
-     execution_role_arn             = aws_iam_role.station_execution_role.arn
-}*/
-
+/** Station back **/
 module "station_back"{
      source = "./station_back"
      application                    = var.application
@@ -21,6 +8,21 @@ module "station_back"{
      station_back_container_cpu     = var.station_back_container_cpu
      station_back_image             = var.station_back_image
      station_back_host_port         = var.station_back_host_port
+     region                         = var.region
+     task_role_arn                  = aws_iam_role.station_iam_role.arn
+     execution_role_arn             = aws_iam_role.station_execution_role.arn
+}
+
+/** Station front **/
+module "station_front"{
+     source = "./station_front"
+     application                    = var.application
+     vpc_id                         = data.aws_vpc.station_vpc.id
+     subnets_id                     = [ data.aws_subnet.station_publicsubnet1.id , data.aws_subnet.station_publicsubnet2.id ]
+     station_front_container_memory = var.station_front_container_memory
+     station_front_container_cpu    = var.station_front_container_cpu
+     station_front_image            = var.station_front_image
+     station_front_host_port        = var.station_front_host_port
      region                         = var.region
      task_role_arn                  = aws_iam_role.station_iam_role.arn
      execution_role_arn             = aws_iam_role.station_execution_role.arn
