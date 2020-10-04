7f747fe7ef8bcfa789dd755551aca2df3bbad4a9
unknown <koko_266@@hotmail.fr>
2020-10-04 16:07:18 +0200

    Change version

--- terraformaws/ecs/main.tf
+++ terraformaws/ecs/main.tf
@@ -4,7 +4,7 @@
      source = "./station_db"
      application                    = var.application
      vpc_id                         = data.aws_vpc.station_vpc.id
-     subnets_id                     = [ data.aws_subnet.station_publicsubnet1.id , data.aws_subnet.station_publicsubnet2.id ]
+     subnets_id                     = [ data.aws_subnet.station_publicsubnet1.id]
      station_db_container_memory    = var.station_db_container_memory
      station_db_container_cpu       = var.station_db_container_cpu
      station_db_image               = var.station_db_image
