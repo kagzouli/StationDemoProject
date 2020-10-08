7fa9950a4e9195efb4b6f56bf82567604dd1122d
root <root@vps620705.ovh.net>
2020-10-08 10:52:37 +0200

    Correction back

--- terraformaws/ecs/station_back/ecs-task-definition.tf
+++ terraformaws/ecs/station_back/ecs-task-definition.tf
@@ -1,7 +1,7 @@
 resource "aws_ecs_task_definition" "station_back_ecs_task_definition"{
-    requires_compatibilities = ["FARGATE"]
     family                      = "station_back_ecs_task_definition"
     network_mode                = "awsvpc"
+    requires_compatibilities    = ["FARGATE"]
     memory                      = var.station_back_container_memory
     cpu                         = var.station_back_container_cpu
     container_definitions       = data.template_file.station_back.rendered
