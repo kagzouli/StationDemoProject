8603d20108f2bffc3646ab12b279683944ddf16a
root <root@vps620705.ovh.net>
2020-10-08 13:00:33 +0200

    Route53

--- terraformaws/ecs/station_back/route53.tf
+++ terraformaws/ecs/station_back/route53.tf
@@ -1,3 +1,9 @@
+# AWS Route 53 Zone
+resource "aws_route53_zone" "primary" {
+  name = "exaka.com"
+}
+
+# AWS Route 53 Record
 resource "aws_route53_record" "station_back_url_external" {
   zone_id = aws_route53_zone.primary.zone_id
   name    = var.station_back_url_external 
