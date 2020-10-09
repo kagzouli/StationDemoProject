36a5d43405b0a405253bcafa0b182820379ddf13
root <root@vps620705.ovh.net>
2020-10-09 12:09:32 +0200

    Correction pour Route53

--- terraformaws/ecs/station_back/route53.tf
+++ terraformaws/ecs/station_back/route53.tf
@@ -1,11 +1,11 @@
 # AWS Route 53 Zone
-resource "aws_route53_zone" "primary" {
+data "aws_route53_zone" "public" {
   name = var.station_domainname 
 }
 
 # AWS Route 53 Record
 resource "aws_route53_record" "station_back_url_external" {
-  zone_id = aws_route53_zone.primary.zone_id
+  zone_id =  data.aws_route53_zone.public.zone_id
   name    = "${var.station_back_url_external}.${var.station_domainname}" 
   type    = "CNAME"
   ttl     = "10"
