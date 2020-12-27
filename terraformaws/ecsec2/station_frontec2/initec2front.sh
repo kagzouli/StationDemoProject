a03907e4e6ad740d6f321260674f34267491467e
root <root@vps620705.ovh.net>
2020-12-27 22:49:09 +0100

    Add space

--- terraformaws/ecsec2/station_frontec2/initec2front.sh
+++ terraformaws/ecsec2/station_frontec2/initec2front.sh
@@ -6,6 +6,7 @@ MIME-Version: 1.0
 Content-Type: text/x-shellscript; charset="us-ascii"
 
 #!/bin/bash
+exec > >(tee /var/log/user-data.log|logger -t user-data -s 2>/dev/console) 2>&1
 set -x
 
 echo ECS_CLUSTER=${ecs_cluster_name} >> /etc/ecs/ecs.config
