7507d3ff5105cfc019a46bb5572408ffc557dbdd
root <root@vps620705.ovh.net>
2020-12-27 23:20:23 +0100

    Correction issue start ecs

--- terraformaws/ecsec2/station_backec2/initec2back.sh
+++ terraformaws/ecsec2/station_backec2/initec2back.sh
@@ -20,8 +20,18 @@ systemctl restart amazon-ssm-agent
 
 sudo yum update -y
 
-#install awscli
-sudo yum install -y awscli
+# Patch
+yum install -y yum-plugin-kernel-livepatch
+yum kernel-livepatch enable -y
+yum update kpatch-runtime -y
+systemctl enable kpatch.service
+amazon-linux-extras enable livepatch
+cat << EOF >
+0 1 * * * sudo yum update --security -y >> /home/ec2-user/security_patches.log 2>&1
+EOF
+chmod 600 /var/spool/cron/ec2-user
+chown ec2-user:ec2-user /var/spool/cron/ec2-user
+sudo systemctl reload crond.service
 
 #Stop the Amazon ECS container agent
 sudo systemctl stop ecs.service
