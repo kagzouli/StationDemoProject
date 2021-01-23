12fb0bf63be3829995b2a55ba7c8896d1d5fb2df
root <root@vps620705.ovh.net>
2021-01-23 10:03:17 +0100

    Init AWS Redis

---
+++ terraformaws/stationredis/aws.tf
@@ -0,0 +1,14 @@
+terraform {
+  required_version = ">= 0.12.18"
+
+  backend "s3" {
+      region  = "eu-west-3" 
+      bucket  = "station-tfstate"
+      key     = "station-redis.tfstate"
+      encrypt = true
+  }
+}
+
+provider "aws" {
+      region     = var.region
+}
