4cc866df46ccdf081930c529d31b328d56261c70
root <root@vps620705.ovh.net>
2021-01-23 10:07:12 +0100

    Init redis AWS

---
+++ terraformaws/stationredis/route3.tf
@@ -0,0 +1,15 @@
+# AWS Route 53 Zone
+data "aws_route53_zone" "private" {
+  name         = var.station_privatedomainname 
+  private_zone = true
+}
+
+# AWS Route 53 Record
+resource "aws_route53_record" "station_redis_url_external" {
+  zone_id      =  data.aws_route53_zone.private.zone_id
+  name         = "${var.station_redis_url_external}.${var.station_privatedomainname}" 
+  type         = "CNAME"
+  ttl          = "10"
+  records      = ["${aws_rds_cluster.station_db_rds_cluster.endpoint}"] 
+ 
+}
