dcb0c975dac49492e8eb8d45b76087a2d01f393b
root <root@vps620705.ovh.net>
2021-01-26 18:53:08 +0100

    Securisation AWS

--- terraformaws/stationredis/redis.tf
+++ terraformaws/stationredis/redis.tf
@@ -8,16 +8,16 @@ resource "aws_elasticache_subnet_group" "station_redis_subnet_group" {
 resource "aws_elasticache_replication_group" "station_redis_cluster" {
   replication_group_id           =  "station-redis-cluster"
   engine                         =  "redis"
-  transit_encryption_enabled     =  false 
   replication_group_description  =  "Redis cluster for caching storage (has automatic eviction)"
   node_type                      =  var.station_redis_instance_type 
- # transit_encryption_enabled     =  true
- # auth_token                     =  var.station_redis_password
+  transit_encryption_enabled     =  true
+  at_rest_encryption_enabled     =  true
+  auth_token                     =  var.station_redis_password
   number_cache_clusters          =  var.station_redis_count
   automatic_failover_enabled     =  false
   availability_zones             =  ["${var.az_zone1}", "${var.az_zone2}"] 
-  security_group_ids             =  ["${aws_security_group.secgroup_station_redis.id}"] 
-  subnet_group_name              =  "${aws_elasticache_subnet_group.station_redis_subnet_group.name}"
+  security_group_ids             =  [aws_security_group.secgroup_station_redis.id] 
+  subnet_group_name              =  aws_elasticache_subnet_group.station_redis_subnet_group.name
   parameter_group_name           =  "default.redis6.x"
   port                           =  var.station_redis_host_port
 
