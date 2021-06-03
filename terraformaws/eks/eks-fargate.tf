81d490d1ed992a64e3a619faae8190f1d83a1984
Ubuntu <ubuntu@vps620705.ovh.net>
2021-06-03 16:16:21 +0000

    Add fargate

--- terraformaws/eks/eks-fargate.tf
+++ terraformaws/eks/eks-fargate.tf
@@ -1,3 +1,14 @@
+resource "kubernetes_namespace" "fargate_namespace" {
+  metadata {
+    labels = {
+      app = "stationback"
+    }
+    name = "fargate-node"
+  }
+}
+
+
+
 resource "aws_eks_fargate_profile" "aws_eks_fargate" {
   cluster_name           = aws_eks_cluster.station_eks_cluster.name
   fargate_profile_name   = "aws-eks-profile-fargate"
@@ -5,7 +16,7 @@ resource "aws_eks_fargate_profile" "aws_eks_fargate" {
   subnet_ids             = [ data.aws_subnet.station_privatesubnet1.id , data.aws_subnet.station_privatesubnet2.id ] 
 
   selector {
-    namespace = "station_namespace"
+    namespace = "fargate-node" 
   }
 
   timeouts {
