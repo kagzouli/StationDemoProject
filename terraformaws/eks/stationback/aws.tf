efd01f7f23759fa5ab97397f8cecf6e8e9c032cc
Ubuntu <ubuntu@vps620705.ovh.net>
2021-06-25 00:03:17 +0000

    Correction version

--- terraformaws/eks/stationback/aws.tf
+++ terraformaws/eks/stationback/aws.tf
@@ -20,6 +20,5 @@ provider "kubernetes" {
   host                   = data.aws_eks_cluster.eks_cluster.endpoint
   cluster_ca_certificate = base64decode(data.aws_eks_cluster.eks_cluster.certificate_authority.0.data)
   token                  = data.aws_eks_cluster_auth.eks_cluster.token
-  load_config_file       = false
   version                = "2.3.2"
 }
