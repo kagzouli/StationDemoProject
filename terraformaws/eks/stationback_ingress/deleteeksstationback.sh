51a8604517407424818844e609f59a341952ca93
Ubuntu <ubuntu@vps620705.ovh.net>
2021-06-26 10:34:19 +0000

    Ajout external-dns

--- terraformaws/eks/stationback_ingress/deleteeksstationback.sh
+++ terraformaws/eks/stationback_ingress/deleteeksstationback.sh
@@ -1,3 +1,5 @@
+kubectl delete -f external-dns.yaml
+
 kubectl delete -f ingress.yaml
 
 terraform destroy -auto-approve --lock=false --var-file=../../vars/dev.tvars 
--- terraformaws/eks/stationback_ingress/generateeksstationback.sh
+++ terraformaws/eks/stationback_ingress/generateeksstationback.sh
@@ -1,6 +1,9 @@
 echo "Apply terraform change"
 terraform apply -auto-approve --lock=false --var-file=../../vars/dev.tvars 
 
+echo "Apply external-dns.yaml"
+kubectl apply -f external-dns.yaml
+
 echo "Apply ingress.yaml"
 kubectl apply -f ingress.yaml
 
