e18c1948073a21ffd7689a63b1cb0856ba573fa9
Ubuntu <ubuntu@vps620705.ovh.net>
2021-06-26 11:03:52 +0000

    External dns role

--- terraformaws/eks/stationback_ingress/eks-iam.tf
+++ terraformaws/eks/stationback_ingress/eks-iam.tf
@@ -24,6 +24,65 @@ resource "aws_iam_role" "aws_eks_fargate_role" {
 POLICY
 }
 
+
+resource "aws_iam_role" "aws_eks_external_dns_role" {
+  name = "aws-eks-external-dns-role"
+  description = "AWS EKS External DNS role"
+   assume_role_policy = <<POLICY
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Effect": "Allow",
+      "Principal": {
+        "Service": [
+          "eks.amazonaws.com",
+          "eks-fargate-pods.amazonaws.com"
+          ]
+      },
+      "Action": "sts:AssumeRole"
+    }
+  ]
+}
+POLICY
+}
+
+
+
+resource "aws_iam_policy" "external_dns_policy" {
+  name        = "external-dns-policy"
+  description = "External dns policy"
+
+  policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Effect": "Allow",
+      "Action": [
+        "route53:ChangeResourceRecordSets"
+      ],
+      "Resource": [
+        "arn:aws:route53:::hostedzone/*"
+      ]
+    },
+    {
+      "Effect": "Allow",
+      "Action": [
+        "route53:ListHostedZones",
+        "route53:ListResourceRecordSets"
+      ],
+      "Resource": [
+        "*"
+      ]
+    }
+  ]
+}
+
+EOF
+}
+
+
 resource "aws_iam_role_policy_attachment" "AmazonEKSFargatePodExecutionRolePolicy" {
   policy_arn = "arn:aws:iam::aws:policy/AmazonEKSFargatePodExecutionRolePolicy"
   role       = aws_iam_role.aws_eks_fargate_role.name
@@ -38,3 +97,10 @@ resource "aws_iam_role_policy_attachment" "AmazonEKSVPCResourceController" {
   policy_arn = "arn:aws:iam::aws:policy/AmazonEKSVPCResourceController"
   role       = aws_iam_role.aws_eks_fargate_role.name
 }
+
+
+resource "aws_iam_role_policy_attachment" "aws_iam_role_attachment_externaldns" {
+  policy_arn = aws_iam_policy.external_dns_policy.arn
+  role       = aws_iam_role.aws_eks_external_dns_role.name
+}
+
