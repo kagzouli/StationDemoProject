3646566e7758b9a69adbfc2a70246999986b88bd
Ubuntu <ubuntu@vps620705.ovh.net>
2021-07-01 16:42:57 +0000

    Correction bucket EFS

--- terraformaws/efs/aws.tf
+++ terraformaws/efs/aws.tf
@@ -4,7 +4,7 @@ terraform {
   backend "s3" {
       region  = "eu-west-3" 
       bucket  = "station-tfstate"
-      key     = "station-ekskubcluster.tfstate"
+      key     = "station-efseks.tfstate"
       encrypt = true
   }
 }
