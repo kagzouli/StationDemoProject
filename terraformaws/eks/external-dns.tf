5fb90ef86fef8d8839b934337017efba74c7240e
Ubuntu <ubuntu@vps620705.ovh.net>
2021-07-23 20:01:46 +0000

    Choose mode database externaldb , internaldb

--- terraformaws/eks/external-dns.tf
+++
@@ -1,7 +0,0 @@
-module "external_dns" {
-  source               = "git::https://github.com/rhythmictech/terraform-aws-eks-iam-external-dns"
-  cluster_name         = aws_eks_cluster.station_eks_cluster.name 
-  issuer_url           = aws_eks_cluster.station_eks_cluster.identity[0].oidc[0].issuer
-  kubernetes_namespace = "kube-system"
-}
-
