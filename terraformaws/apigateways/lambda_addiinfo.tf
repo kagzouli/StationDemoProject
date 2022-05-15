1c5f4c3a173f51e19a368569bed1ce2cd117409a
Ubuntu <ubuntu@vps620705.ovh.net>
2022-05-15 22:11:53 +0000

    Correction get method

--- terraformaws/apigateways/lambda_addiinfo.tf
+++ terraformaws/apigateways/lambda_addiinfo.tf
@@ -1,10 +1,10 @@
-###############################################################
+#ws_lambda_function##############################################################
 ############ adddinfo : Get method                           ##
 ###############################################################
 resource "aws_lambda_function" "addiinfo_get" {
   filename         = "addiinfo.zip"
   function_name    = "station-lambda-addiinfo"
-  handler          = "addiinfo.get"
+  handler          = "addiinfo.get_handler"
   role             = aws_iam_role.lambda_iam_role.arn
   runtime          = "python3.9"
   source_code_hash = data.archive_file.addiinfo.output_base64sha256
@@ -27,7 +27,7 @@ resource "aws_lambda_permission" "lambda_addiinfo_get" {
 
 data "archive_file" "addiinfo" {
   type        = "zip"
-  source_file = "lambda/addiinfo_function.py"
+  source_file = "addiinfo.py"
   output_path = "addiinfo.zip"
 }
 
