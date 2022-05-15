f7acc4f8612da24c42d3a0eb033a65e452b89940
Ubuntu <ubuntu@vps620705.ovh.net>
2022-05-15 21:41:17 +0000

    Init lambda

--- terraformaws/apigateways/lambda_addiinfo.tf
+++ terraformaws/apigateways/lambda_addiinfo.tf
@@ -16,35 +16,4 @@ data "archive_file" "addiinfo" {
   output_path = "addiinfo.zip"
 }
 
-resource "aws_iam_role" "lambda_iam_role" {
-  name = "lambda-iam-role"
-
-  assume_role_policy = <<EOF
-{
-  "Version": "2012-10-17",
-  "Statement": [
-    {
-      "Action": "sts:AssumeRole",
-      "Principal": {
-        "Service": "lambda.amazonaws.com"
-      },
-      "Effect": "Allow",
-      "Sid": ""
-    }
-  ]
-}
-EOF
-
-tags = {
-      Name = "lambda-iam-role"
-      Application= var.application
-  }
-
- 
-}
-
-resource "aws_iam_role_policy_attachment" "lambda_attachment_standard" {
-  role       = aws_iam_role.lambda_iam_role.name
-  policy_arn = "arn:aws:iam::aws:policy/service-role/AWSLambdaBasicExecutionRole"
-}
 
