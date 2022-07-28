ae587e77ac1d414ceda230e0de049363a762a5eb
Ubuntu <ubuntu@vps620705.ovh.net>
2022-07-28 18:23:03 +0000

    Modif secrets

--- terraformaws/secrets/providers.tf
+++ terraformaws/secrets/providers.tf
@@ -1,10 +1,8 @@
 terraform {
-  required_version = ">= 0.12.18"
+  required_version = ">= 0.15.5"
 
   backend "s3" {
       region  = "eu-west-3" 
-      bucket  = "station-tfstate"
-      key     = "station-secrets.tfstate"
       encrypt = true
   }
 }
