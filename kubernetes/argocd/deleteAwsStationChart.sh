e6c7fb31d24891eeee53b5ddc2f8738c65a083e0
Karim GZOULI <kagzouli@yahoo.fr>
2022-07-29 01:08:56 +0200

    Correction suppression applications

--- kubernetes/argocd/deleteAwsStationChart.sh
+++ kubernetes/argocd/deleteAwsStationChart.sh
@@ -5,8 +5,8 @@ AWS_REGION="eu-west-3"
 
 aws eks --region ${AWS_REGION} update-kubeconfig --name station-eks-cluster
 
-echo "Suppression du chart station"
-helm delete stationdev -n stationdev
+echo "Suppression du chart applications"
+helm delete applications
 
 echo "Delete transverse namespace and chart helm"
 helm delete transverse  -n ${SHARED_NAMESPACE}
