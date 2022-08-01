cc06d08f6e2355c1ccac544fa8ff96112eb5432f
Karim GZOULI <kagzouli@yahoo.fr>
2022-08-01 22:06:22 +0200

    Creation ECR

--- terraformaws/ecr/ecr.tf
+++ terraformaws/ecr/ecr.tf
@@ -1,4 +1,9 @@
-# Registry database
+locals {
+  role_arn = "arn:aws:iam::${data.aws_caller_identity.current.account_id}:role/devops-ci"
+}
+
+
+################### Registry database ######################################################################
 resource "aws_ecr_repository" "station_database_registry" {
     name  = "station-ecr-registry/station-db"
     image_tag_mutability = var.image_tag_mutability
@@ -21,10 +26,10 @@ resource "aws_ecr_repository_policy" "station_database_registry_policy" {
     "Version": "2008-10-17",
     "Statement": [
       {
-        "Sid": "private access for ${var.images_name[count.index]} repository",
+        "Sid": "private access for db repository",
         "Effect": "Allow",
         "Principal": 
-          {"AWS": "${local.arn}" },
+          {"AWS": "${local.role_arn}" },
         "Action": [
           "ecr:BatchCheckLayerAvailability",
           "ecr:BatchGetImage",
@@ -42,3 +47,96 @@ resource "aws_ecr_repository_policy" "station_database_registry_policy" {
   EOF
     
 }
+
+##################### Registry back ##############################################################
+resource "aws_ecr_repository" "station_back_registry" {
+    name  = "station-ecr-registry/station-back"
+    image_tag_mutability = var.image_tag_mutability
+    image_scanning_configuration {
+      scan_on_push = true
+    }
+  
+    tags = {
+     Name = "station-back-ecr"
+     Application= var.application
+    }
+
+}
+
+resource "aws_ecr_repository_policy" "station_back_registry_policy" {
+  repository = aws_ecr_repository.station_back_registry.name
+
+  policy     = <<EOF
+  {
+    "Version": "2008-10-17",
+    "Statement": [
+      {
+        "Sid": "private access for back repository",
+        "Effect": "Allow",
+        "Principal": 
+          {"AWS": "${local.role_arn}" },
+        "Action": [
+          "ecr:BatchCheckLayerAvailability",
+          "ecr:BatchGetImage",
+          "ecr:CompleteLayerUpload",
+          "ecr:GetDownloadUrlForLayer",
+          "ecr:GetLifecyclePolicy",
+          "ecr:InitiateLayerUpload",
+          "ecr:PutImage",
+          "ecr:UploadLayerPart"
+        ]
+      }
+    ]
+  
+}
+  EOF
+    
+}
+
+
+##################### Registry front ##############################################################
+resource "aws_ecr_repository" "station_front_registry" {
+    name  = "station-ecr-registry/station-front-nginx"
+    image_tag_mutability = var.image_tag_mutability
+    image_scanning_configuration {
+      scan_on_push = true
+    }
+  
+    tags = {
+     Name = "station-front-nginx-ecr"
+     Application= var.application
+    }
+
+}
+
+resource "aws_ecr_repository_policy" "station_front_registry_policy" {
+  repository = aws_ecr_repository.station_front_registry.name
+
+  policy     = <<EOF
+  {
+    "Version": "2008-10-17",
+    "Statement": [
+      {
+        "Sid": "private access for front repository",
+        "Effect": "Allow",
+        "Principal": 
+          {"AWS": "${local.role_arn}" },
+        "Action": [
+          "ecr:BatchCheckLayerAvailability",
+          "ecr:BatchGetImage",
+          "ecr:CompleteLayerUpload",
+          "ecr:GetDownloadUrlForLayer",
+          "ecr:GetLifecyclePolicy",
+          "ecr:InitiateLayerUpload",
+          "ecr:PutImage",
+          "ecr:UploadLayerPart"
+        ]
+      }
+    ]
+  
+}
+  EOF
+    
+}
+
+
