076a60556ab449552b2ce3ae771d4e99361a641f
Karim GZOULI <kagzouli@yahoo.fr>
2022-08-02 23:41:08 +0200

    Correction user

--- terraformaws/user/devops-ci.tf
+++ terraformaws/user/devops-ci.tf
@@ -1,58 +1,3 @@
-###### Policy #############################################################
-resource "aws_iam_role_policy" "devops_ci" {
-  name = "devops-ci"
-  role = aws_iam_role.test_role.id
-
-  # Terraform's "jsonencode" function converts a
-  # Terraform expression result to valid JSON syntax.
-  policy = templatefile("policies/devops-ci-role-policy.json.tpl",
-                    { 
-                       ROLE_TO_ASSUME_DEVOPS_CI = "arn:aws:iam::${data.aws_caller_identity.current.account_id}:role/devops-ci"   
-                    }
-           ) 
-}
-
-
-###### Role ###############################################################
-resource "aws_iam_role" "devops_ci" {
-  name = "devops-ci"
-
-  assume_role_policy = jsonencode({
-    "Version" : "2012-10-17",
-    "Statement" : [
-      {
-        "Action" : "sts:AssumeRole",
-        "Principal" : {
-          "Service" : "apigateway.amazonaws.com"
-        },
-        "Effect" : "Allow",
-        "Sid" : ""
-      }
-    ]
-  })
-}
-
-
-
-####### Group devops-ci ###################################################
-resource "aws_iam_group" "devops_ci" {
-  name = "devops-ci"
-}
-
-
-resource "aws_iam_group_policy" "devops_ci" {
-  name  = "devops-ci"
-  group = aws_iam_group.devops_ci.name
-
-  policy = templatefile("policies/devops-ci-group.json.tpl",
-                    { 
-                       ROLE_TO_ASSUME_DEVOPS_CI = "arn:aws:iam::${data.aws_caller_identity.current.account_id}:role/devops-ci"   
-                    }
-           ) 
-}
-
-
-
 ############### User devops-ci ###########################################
 resource "aws_iam_user" "devops_ci" {
   name = "devops-ci"
@@ -66,6 +11,11 @@ resource "aws_iam_user" "devops_ci" {
   }
 }
 
+############### Group devops-ci#########################################
+
+resource "aws_iam_group" "devops_ci" {
+  name = "devops-ci"
+}
 
 resource "aws_iam_user_group_membership" "devops_ci" {
   user =  aws_iam_user.devops_ci.name 
@@ -74,6 +24,61 @@ resource "aws_iam_user_group_membership" "devops_ci" {
     aws_iam_group.devops_ci.name
   ]
 }
+
+############ Role policy ##############################################
+data "aws_iam_policy_document" "devops_ci_trust_relationship" {
+  statement {
+    actions = [
+        "sts:AssumeRole",
+        "sts:TagSession"
+    ]
+    effect  = "Allow"
+
+    principals {
+      identifiers = [aws_iam_user.devops_ci.arn]
+      type        = "AWS"
+    }
+  }
+
+}
+
+
+###### Role ###############################################################
+resource "aws_iam_role" "devops_ci" {
+  name = "devops-ci"
+
+  assume_role_policy = data.aws_iam_policy_document.devops_ci_trust_relationship.json
+}
+
+
+###### Policy #############################################################
+resource "aws_iam_role_policy" "devops_ci" {
+  name = "devops-ci"
+  role = aws_iam_role.devops_ci.id
+
+  policy = templatefile("policies/devops-ci-role-policy.json.tpl",
+                    { 
+                      
+                    }
+           ) 
+}
+
+
+
+
+####### Group Policy devops-ci ###################################################
+
+resource "aws_iam_group_policy" "devops_ci" {
+  name  = "devops-ci"
+  group = aws_iam_group.devops_ci.name
+
+  policy = templatefile("policies/devops-ci-group.json.tpl",
+                    { 
+                       ROLE_TO_ASSUME_DEVOPS_CI = aws_iam_role.devops_ci.arn 
+                    }
+           ) 
+}
+
 
 
 
