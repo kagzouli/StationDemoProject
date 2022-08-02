4866f9423558cb851f3d9e856c6ff3be768af96d
Karim GZOULI <kagzouli@yahoo.fr>
2022-08-02 23:23:18 +0200

    Ajout user

--- terraformaws/user/devops-ci.tf
+++ terraformaws/user/devops-ci.tf
@@ -1,15 +1,50 @@
+###### Policy #############################################################
+resource "aws_iam_role_policy" "devops_ci" {
+  name = "devops-ci"
+  role = aws_iam_role.test_role.id
+
+  # Terraform's "jsonencode" function converts a
+  # Terraform expression result to valid JSON syntax.
+  policy = templatefile("policies/devops-ci-role-policy.json.tpl",
+                    { 
+                       ROLE_TO_ASSUME_DEVOPS_CI = "arn:aws:iam::${data.aws_caller_identity.current.account_id}:role/devops-ci"   
+                    }
+           ) 
+}
+
+
+###### Role ###############################################################
+resource "aws_iam_role" "devops_ci" {
+  name = "devops-ci"
+
+  assume_role_policy = jsonencode({
+    "Version" : "2012-10-17",
+    "Statement" : [
+      {
+        "Action" : "sts:AssumeRole",
+        "Principal" : {
+          "Service" : "apigateway.amazonaws.com"
+        },
+        "Effect" : "Allow",
+        "Sid" : ""
+      }
+    ]
+  })
+}
+
+
 
 ####### Group devops-ci ###################################################
-resource "aws_iam_group" "station_devops_ci" {
+resource "aws_iam_group" "devops_ci" {
   name = "devops-ci"
 }
 
 
 resource "aws_iam_group_policy" "devops_ci" {
   name  = "devops-ci"
-  group = aws_iam_group.station_devops_ci.name
+  group = aws_iam_group.devops_ci.name
 
-  policy = templatefile("policies/devops-ci-group-policies.json.tpl",
+  policy = templatefile("policies/devops-ci-group.json.tpl",
                     { 
                        ROLE_TO_ASSUME_DEVOPS_CI = "arn:aws:iam::${data.aws_caller_identity.current.account_id}:role/devops-ci"   
                     }
@@ -19,7 +54,7 @@ resource "aws_iam_group_policy" "devops_ci" {
 
 
 ############### User devops-ci ###########################################
-resource "aws_iam_user" "station_devops_ci" {
+resource "aws_iam_user" "devops_ci" {
   name = "devops-ci"
 
   # Comme on est en test , on met true
@@ -32,11 +67,11 @@ resource "aws_iam_user" "station_devops_ci" {
 }
 
 
-resource "aws_iam_user_group_membership" "station_devops_ci" {
-  user =  aws_iam_user.station_devops_ci.name 
+resource "aws_iam_user_group_membership" "devops_ci" {
+  user =  aws_iam_user.devops_ci.name 
  
   groups = [
-    aws_iam_group.station_devops_ci.name
+    aws_iam_group.devops_ci.name
   ]
 }
 
