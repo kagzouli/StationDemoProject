9b639f333cecdfc246d8f29ecb010b99730a2a10
Karim GZOULI <kagzouli@yahoo.fr>
2022-08-03 22:21:27 +0200

    Correction user devops

--- terraformaws/user/devops.tf
+++ terraformaws/user/devops.tf
@@ -1,27 +1,27 @@
 ############### User devops-ci ###########################################
-resource "aws_iam_user" "devops_ci" {
-  name = "devops-ci"
+resource "aws_iam_user" "devops_user" {
+  name = "devops"
 
   # Comme on est en test , on met true
   force_destroy  = true
 
   tags = {
-     Name = "devops-ci"
+     Name = "devops"
      Application= var.application
   }
 }
 
 ############### Group devops-ci#########################################
 
-resource "aws_iam_group" "devops_ci" {
-  name = "devops-ci"
+resource "aws_iam_group" "devops_role" {
+  name = "devops"
 }
 
-resource "aws_iam_user_group_membership" "devops_ci" {
-  user =  aws_iam_user.devops_ci.name 
+resource "aws_iam_user_group_membership" "devops_member" {
+  user =  aws_iam_user.devops_user.name 
  
   groups = [
-    aws_iam_group.devops_ci.name
+    aws_iam_group.devops_role.name
   ]
 }
 
@@ -35,7 +35,7 @@ data "aws_iam_policy_document" "devops_ci_trust_relationship" {
     effect  = "Allow"
 
     principals {
-      identifiers = [aws_iam_user.devops_ci.arn]
+      identifiers = [aws_iam_user.devops_user.arn]
       type        = "AWS"
     }
   }
@@ -69,10 +69,10 @@ resource "aws_iam_role_policy" "devops_ci" {
 ####### Group Policy devops-ci ###################################################
 
 resource "aws_iam_group_policy" "devops_ci" {
-  name  = "devops-ci"
-  group = aws_iam_group.devops_ci.name
+  name  = "devops"
+  group = aws_iam_group.devops_role.name
 
-  policy = templatefile("policies/devops-ci-group.json.tpl",
+  policy = templatefile("policies/devops-group.json.tpl",
                     { 
                        ROLE_TO_ASSUME_DEVOPS_CI = aws_iam_role.devops_ci.arn 
                     }
