8b482fa904a2a13b601c332adec5a98864fc9bf7
Karim GZOULI <kagzouli@yahoo.fr>
2022-08-05 23:05:28 +0200

    Fixe version providers aws

--- terraformaws/apigateways/providers.tf
+++ terraformaws/apigateways/providers.tf
@@ -5,6 +5,14 @@ terraform {
       region  = "eu-west-3" 
       encrypt = true
   }
+
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+      version = "~> 4.25.0"
+    }
+  
+  }
 }
 
 provider "aws" {
