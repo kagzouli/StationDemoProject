e35a56c69ebdabf13e6279a7f8a38c0694f03620
Karim GZOULI <kagzouli@yahoo.fr>
2023-01-24 20:20:28 +0100

    MAJ worker regle backend

--- terraformaws/clusterkubmanual/worker/deleteworker.sh
+++ terraformaws/clusterkubmanual/worker/deleteworker.sh
@@ -1,1 +1,4 @@
-terraform destroy -auto-approve --lock=false --var-file=../../vars/dev.tvars 
+CPT_NAME="dev"
+rm -rf .terraform
+terraform init --backend-config="backends/${CPT_NAME}_backend.tfvars"
+terraform destroy -auto-approve  --var-file=../../vars/dev.tvars 
