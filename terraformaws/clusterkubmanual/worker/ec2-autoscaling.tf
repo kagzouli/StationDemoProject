5c92bbf670890e3ad05fb70a730414e55813f8b2
Karim GZOULI <kagzouli@yahoo.fr>
2023-01-25 19:52:30 +0100

    Correction pour faire marcher calico

--- terraformaws/clusterkubmanual/worker/ec2-autoscaling.tf
+++ terraformaws/clusterkubmanual/worker/ec2-autoscaling.tf
@@ -11,7 +11,7 @@ resource "aws_launch_configuration" "kubworker_launch_config" {
     iam_instance_profile = data.aws_iam_instance_profile.kubworkermanual_agent.name
     security_groups      = [aws_security_group.kubworkermanual_sg.id]
     user_data            = data.template_file.user_data.rendered 
-    instance_type        = "t2.small"
+    instance_type        = "t3.large"
     associate_public_ip_address = false 
     key_name             = "kubernetesmaster-key-pair" 
  
@@ -26,9 +26,9 @@ resource "aws_autoscaling_group" "kubworker_autoscalinggroup" {
     vpc_zone_identifier       = [data.aws_subnet.station_privatesubnet1.id, data.aws_subnet.station_privatesubnet2.id] 
     launch_configuration      = aws_launch_configuration.kubworker_launch_config.name
 
-    desired_capacity          = 2
-    min_size                  = 2 
-    max_size                  = 2 
+    desired_capacity          = 1
+    min_size                  = 1 
+    max_size                  = 1 
     health_check_grace_period = 300
     health_check_type         = "EC2"
 
