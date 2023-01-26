349ebba1c959ba8b0b2cb491e6bc19fc46397458
Karim GZOULI <kagzouli@yahoo.fr>
2023-01-26 18:46:02 +0100

    Add correction ip

--- kubernetesaws/helm/deleteStationChart.sh
+++ kubernetesaws/helm/deleteStationChart.sh
@@ -4,4 +4,9 @@ helm delete argo-rollout  -n transverse
 
 helm delete metrics-server  -n transverse
 
+helm upgrade --install ingress-nginx -n transverse
+
+kubectl delete -f https://raw.githubusercontent.com/kubernetes/ingress-nginx/controller-0.32.0/deploy/static/provider/aws/deploy.yaml
+
+
 
