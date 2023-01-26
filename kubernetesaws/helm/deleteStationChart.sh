55ca17699a08466a582bcbc6c762951890ef9053
Karim GZOULI <kagzouli@yahoo.fr>
2023-01-26 18:47:35 +0100

    Add correction ip

--- kubernetesaws/helm/deleteStationChart.sh
+++ kubernetesaws/helm/deleteStationChart.sh
@@ -6,6 +6,9 @@ helm delete metrics-server  -n transverse
 
 helm upgrade --install ingress-nginx -n transverse
 
+
+kubectl delete -f https://raw.githubusercontent.com/metallb/metallb/v0.13.7/config/manifests/metallb-native.yaml
+
 kubectl delete -f https://raw.githubusercontent.com/kubernetes/ingress-nginx/controller-0.32.0/deploy/static/provider/aws/deploy.yaml
 
 
