2ccf9a295b69a4f3c8052330eb8499641a7c59fc
Karim GZOULI <kagzouli@yahoo.fr>
2023-01-26 11:16:22 +0100

    Correction helm chart metrics

--- kubernetesaws/helm/launchStationChart.sh
+++ kubernetesaws/helm/launchStationChart.sh
@@ -1,7 +1,7 @@
 kubectl create namespace transverse --dry-run=client -o yaml | kubectl apply -f -
 mkdir -p /var/lib/mysql 
 helm repo add metrics-server https://kubernetes-sigs.github.io/metrics-server/
-helm upgrade --install metrics-server metrics-server/metrics-server --version 3.8.2 -n transverse
+helm upgrade --install metrics-server metrics-server/metrics-server --version 3.8.2 --set  args[0]="--kubelet-insecure-tls=true" -n transverse
 helm repo add argo https://argoproj.github.io/argo-helm
 helm upgrade --install argo-rollout argo/argo-rollouts --version 2.21.3 --set installCRDs=true -n transverse
 helm install stationdev ./station -n stationdev --create-namespace
