881362669288c505d0489736eef0eda4b3ed3c40
Karim GZOULI <kagzouli@yahoo.fr>
2023-10-20 01:29:06 +0200

    Build

---
+++ loadTest/python3/setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup
+import ssl
+ssl._create_default_https_context = ssl._create_unverified_context
+
+setup(
+    name='loadtest',
+    version='1.0.0',
+    description='Library for load test',
+    author='Exaka Consulting',
+    author_email='exa@exaka.com',
+    license="Unlicensed",
+    packages=[''],
+    python_requires=">=3.11",
+    install_requires=[
+        "requests", "kubernetes"
+    ], #external packages as dependencies
+    entry_points={
+        "console_scripts": [
+            "loadtest=main.__main__:main"
+        ]
+    },
+    include_package_data=True,
+    package_data={"" : ["utils/*"]},
+    zip_safe=False
+)
