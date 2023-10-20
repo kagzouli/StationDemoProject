188f0281e3140bd24d91b3433686370c29a41f76
Karim GZOULI <kagzouli@yahoo.fr>
2023-10-20 16:48:40 +0200

    Correction

--- loadTest/python3/loadtest/utils/prometheus.py
+++ loadTest/python3/loadtest/utils/prometheus.py
@@ -7,14 +7,17 @@ PROMETHEUS_API_URL = "http://prometheus.exakaconsulting.org/api/v1"
 # Method for container usage memory
 def container_memory_usage(namespace_name: str, container_name: str, start_time: int, end_time: int,
                            step: int) -> List[float]:
-    url: str = (
+    url_memory_usage: str = (
             f"{PROMETHEUS_API_URL}/query_range?query=sum(container_memory_working_set_bytes" +
             "{namespace=%22" + f"{namespace_name}%22, container=%22{container_name}%22" +
             "}" +
             f")&start={start_time}&end={end_time}&step={step}"
     )
 
-    response = requests.get(url, verify=False)
+    print(f"URL memory usage : {url_memory_usage}")
+
+    # Lancement requete
+    response = requests.get(url_memory_usage, verify=False)
     # Si c ok
     if response.status_code == 200:
         result_json = response.json()
@@ -31,7 +34,7 @@ def container_memory_usage(namespace_name: str, container_name: str, start_time:
 # Method for container usage cpu
 def container_cpu_usage(namespace_name: str, container_name: str, start_time: int, end_time: int,
                         step: int) -> List[float]:
-    url: str = (
+    url_cpu_usage: str = (
             f"{PROMETHEUS_API_URL}/query_range?query=sum(" +
             f"node_namespace_pod_container:container_cpu_usage_seconds_total:sum_irate" +
             "{namespace=%22" + f"{namespace_name}%22, container=%22{container_name}%22" +
@@ -39,7 +42,10 @@ def container_cpu_usage(namespace_name: str, container_name: str, start_time: in
             f")&start={start_time}&end={end_time}&step={step}"
     )
 
-    response = requests.get(url, verify=False)
+    print(f"URL cpu usage : {url_cpu_usage}")
+
+    # Lancement requete
+    response = requests.get(url_cpu_usage, verify=False)
     if response.status_code == 200:
         result_json = response.json()
         values = [
