27aea530946c9d4fe0db47b6bc0e4e08a1ade904
Karim GZOULI <kagzouli@yahoo.fr>
2023-10-20 16:13:54 +0200

    Correction prometheus

--- loadTest/python3/loadtest/cli.py
+++ loadTest/python3/loadtest/cli.py
@@ -20,25 +20,36 @@ def main(
         token: str = typer.Option(default="token", help="The token for test"),
         namespace_name: str = typer.Option(default="", help="The Namespace name"),
         container_name: str = typer.Option(default="", help="The Container name"),
-        load_file : str=typer.Option(default="data.txt" , help = "The load files for the data.")
+        load_file: str = typer.Option(default="data.txt", help="The load files for the data.")
 ):
-
     # Start the timer before the test
     start_time = int(time.time())
 
     # Launch the test with k6
-    k6.run_test(url , nbr_users , duration , wait_time  , token, load_file )
+    k6.run_test(url, nbr_users, duration, wait_time, token, load_file)
 
     # End the timer after the test + 30 seconds
     today_date = datetime.datetime.now(datetime.UTC)
     end_date_datetime = today_date + relativedelta(second=30)
-    end_time = get_epochtime_ms((end_date_datetime))
+    end_time = get_epochtime_ms(end_date_datetime)
 
     print(f"Start time : {start_time} , End time : {end_time}")
 
-    prometheus.container_usage_memory(namespace_name, container_name, start_time, end_time, 1)
+    # Get usage memory
+    memory_usage = prometheus.container_memory_usage(namespace_name, container_name, start_time, end_time, 1)
+    memory_min = min(memory_usage)
+    memory_max = max(memory_usage)
+    memory_avg = sum(memory_usage) / len(memory_usage)
+    print(f"Utilisation memoire min : {memory_min} // Memoire max : {memory_max} // Memory avg : {memory_avg}")
+
+    # Get CPU usage
+    cpu_usage = prometheus.container_cpu_usage(namespace_name, container_name, start_time, end_time, 1)
+    cpu_min = min(cpu_usage)
+    cpu_max = max(cpu_usage)
+    cpu_avg = sum(cpu_usage) / len(cpu_usage)
+    print(f"Utilisation CPU min : {cpu_min} // CPU max : {cpu_max} // CPU avg : {cpu_avg}")
 
 
-def get_epochtime_ms(date_time : datetime):
+def get_epochtime_ms(date_time: datetime) -> int:
     epochtime = int(round(date_time.timestamp()))
     return epochtime
