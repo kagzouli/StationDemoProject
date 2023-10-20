b00a35bb61fce217093d6b1b0ce32429b7b7db36
Karim GZOULI <kagzouli@yahoo.fr>
2023-10-20 14:27:07 +0200

    End time

--- loadTest/python3/loadtest/cli.py
+++ loadTest/python3/loadtest/cli.py
@@ -1,4 +1,6 @@
+import datetime
 import time
+from dateutil.relativedelta import relativedelta
 import typer
 from loadtest.utils import prometheus
 from loadtest.utils.k6 import k6
@@ -21,10 +23,22 @@ def main(
         load_file : str=typer.Option(default="data.txt" , help = "The load files for the data.")
 ):
 
-    # Launch the test
+    # Start the timer before the test
+    start_time = int(time.time())
+
+    # Launch the test with k6
     k6.run_test(url , nbr_users , duration , wait_time  , token, load_file )
 
-    start_time = int(time.time())
-    end_time = int(time.time())
+    # End the timer after the test + 30 seconds
+    today_date = datetime.datetime.now(datetime.UTC)
+    end_date_datetime = today_date + relativedelta(second=30)
+    end_time = get_epochtime_ms((end_date_datetime))
+
+    print(f"Start time : {start_time} , End time : {end_time}")
 
     prometheus.container_usage_memory(namespace_name, container_name, start_time, end_time, 1)
+
+
+def get_epochtime_ms(date_time : datetime):
+    epochtime = int(round(date_time.timestamp()))
+    return epochtime
