f8ca594232b49fc1fa829aa41243d70f505526b6
Karim GZOULI <kagzouli@yahoo.fr>
2023-10-24 00:28:18 +0200

    Ajout shared

--- kubernetesonprem/deleteStation.sh
+++ kubernetesonprem/deleteStation.sh
@@ -1,3 +1,5 @@
 ARGO_NAMESPACE="argocd"
 
+helm delete shared
+
 helm delete argocd  -n ${ARGO_NAMESPACE}
--- kubernetesonprem/launchStation.sh
+++ kubernetesonprem/launchStation.sh
@@ -4,11 +4,15 @@ ARGO_NAMESPACE="argocd"
 helm repo add argo https://argoproj.github.io/argo-helm
 helm repo update
 
-# Install argocd
+# Install argocd - Mot de passe argocd123 (bcrypt hash)
 helm upgrade --install argocd argo/argo-cd --set server.ingress.enabled=true \
     --set server.ingress.annotations."kubernetes\.io/ingress\.class"=nginx \
     --set server.ingress.annotations."nginx\.ingress\.kubernetes\.io/force-ssl-redirect"="true" \
     --set server.ingress.annotations."nginx\.ingress\.kubernetes\.io/backend-protocol"="HTTPS" \
-    --set configs.secret.argocdServerAdminPassword="argocd123" \
-    --set configs.secret.argocdServerAdminPasswordMtime="2023-10-23T10:11:12Z" \
+    --set configs.secret.argocdServerAdminPassword='$2y$10$8LaHsCQS1hd74MIYj4jinuVni14FBL4PlDJ0SKAg9f/pQvolxVlRS' \
+    --set configs.secret.argocdServerAdminPasswordMtime="2023-10-01T10:11:12Z" \
     --set server.ingress.hosts[0]="argocd.exakaconsulting.org" -n ${ARGO_NAMESPACE} --create-namespace
+
+# Install shared
+helm upgrade --install shared ./argocd/shared
+
