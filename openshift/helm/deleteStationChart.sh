5e69fc24f4c91987ef3c414e961d7ec144fe5478
kagzouli <karim@exakaconsulting.com>
2025-11-15 14:44:13 +0100

    Adapt for openshift

--- openshift/helm/deleteStationChart.sh
+++ openshift/helm/deleteStationChart.sh
@@ -5,38 +5,19 @@ FALCO_NAMESPACE="falco"
 
 
 
-helm delete stationdev -n stationdev
-kubectl delete secret vault-secret -n stationdev
+STATION_PROJECT_NAME="stationdev"
+oc project $STATION_PROJECT_NAME
 
-helm delete vault -n ${VAULT_MONITORING} 
+# Delete all standard resources
+oc delete all --all
 
-helm delete cadvisor -n ${MONITORING_NAMESPACE}
-
-helm delete prometheus-operator-crds -n ${MONITORING_NAMESPACE}
-
-
-helm delete falco -n ${FALCO_NAMESPACE}
-
-
-helm delete kube-state-metrics -n ${MONITORING_NAMESPACE}     
-
-helm delete prometheus   -n ${MONITORING_NAMESPACE}
-
-helm delete keda -n ${SHARED_NAMESPACE}
-
-helm delete argo-rollout  -n ${SHARED_NAMESPACE}
-
-helm delete metrics-server  -n ${SHARED_NAMESPACE}
-
-helm delete external-secrets -n ${SHARED_NAMESPACE}
-
-
-kubectl delete secret vault-secret -n stationdev
-
-
-kubectl delete -f https://raw.githubusercontent.com/metallb/metallb/v0.13.7/config/manifests/metallb-native.yaml
-
-kubectl delete -f https://raw.githubusercontent.com/kubernetes/ingress-nginx/controller-0.32.0/deploy/static/provider/aws/deploy.yaml
-
-
-
+# Delete other resources
+oc delete configmap --all
+oc delete secret --all
+oc delete route --all
+oc delete pvc --all
+oc delete serviceaccount --all
+oc delete rolebinding --all
+oc delete role --all
+oc delete quota --all
+oc delete limitrange --all
