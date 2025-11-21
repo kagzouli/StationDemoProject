259533e433122d0ddb301758cef8fed4fb208c12
kagzouli <karim@exakaconsulting.com>
2025-11-21 21:27:27 +0100

    Host openshift

--- xldeploy/startxld.sh
+++ xldeploy/startxld.sh
@@ -1,3 +1,4 @@
+# Host for CRC openshift : https://host.docker.internal:6443
 docker run -d -p 4516:4516 \
 -v C:/Temp/XebiaLabs/xl-deploy-docker/conf:/opt/xebialabs/xl-deploy-server/conf:rw \
 -v C:/Temp/XebiaLabs/xl-deploy-docker/repository:/opt/xebialabs/xl-deploy-server/repository:rw \
