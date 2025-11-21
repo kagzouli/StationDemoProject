a72036ad13f1ad90aea08144066edfe01b685043
kagzouli <karim@exakaconsulting.com>
2025-11-21 21:04:48 +0100

    Script Init XLDeploy

--- xldeploy/startxld.sh
+++ xldeploy/startxld.sh
@@ -1,6 +1,6 @@
 docker run -d -p 4516:4516 \
--v C:/Karim/XebiaLabs/xl-deploy-docker/conf:/opt/xebialabs/xl-deploy-server/conf:rw \
--v C:/Karim/XebiaLabs/xl-deploy-docker/repository:/opt/xebialabs/xl-deploy-server/repository:rw \
--v C:/Karim/XebiaLabs/xl-deploy-docker/archive:/opt/xebialabs/xl-deploy-server/archive:rw \
+-v C:/Temp/XebiaLabs/xl-deploy-docker/conf:/opt/xebialabs/xl-deploy-server/conf:rw \
+-v C:/Temp/XebiaLabs/xl-deploy-docker/repository:/opt/xebialabs/xl-deploy-server/repository:rw \
+-v C:/Temp/XebiaLabs/xl-deploy-docker/archive:/opt/xebialabs/xl-deploy-server/archive:rw \
 -e ACCEPT_EULA=Y \
 --name xld xebialabsearlyaccess/xl-deploy:25.3.0
