5fff3264822beccd8cba00d452356ac6a3c3a6df
kagzouli <karim@exakaconsulting.com>
2025-12-06 22:38:57 +0100

    Correction Auth0

--- application/StationDemoClient/station-angular4-poc/src/app/app.module.ts
+++ application/StationDemoClient/station-angular4-poc/src/app/app.module.ts
@@ -42,16 +42,17 @@ export function authConfigFactory(configService: ConfigurationLoaderService) {
   const cfg =  {
     domain: oktaUrl,
     clientId: configService.get('clientIdTrafStat'),
-    redirectUri: window.location.origin + "/station-angular4-poc/",
     useRefreshTokens: true,   // <-- enable refresh tokens
     cacheLocation: 'localstorage', // required if using refresh tokens
     useRefreshTokensFallback: false, // optional, but recommended
     audience: `https://${oktaUrl}/api/v2/`,
     scope: "openid profile email offline_access api.read",
     authorizationParams: {
+        redirect_uri: window.location.origin + "/station-angular4-poc/",
         scope: "openid profile email offline_access api.read"
     }
   };
+  
 
   return {
     get: () => cfg // provide the old `.get()` API
